06071831762b9fe00578a1433dcf79a1c645c0e3
Sai Purciful <saipurciful@gmail.com>
2021-01-31 23:31:42 -0800

    added folders in src

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <div className="container about" id="about" style="margin-top: 0;">
+                <h1>About Me</h1>
+                <div class="about-me-info">
+                    <p>Successful professional with a bachelor’s degree in hotel management, two years of experience in
+                large-scale hotel management, and three years of experience as a licensed massage therapist. </p>
+                    <p> Seeking a career change to a full-stack web developer for a fast-paced company with big goals that I can
+                    be a part of. Driven self-starter and lifelong learner who tackles difficult tasks with a team-player
+                attitude.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
