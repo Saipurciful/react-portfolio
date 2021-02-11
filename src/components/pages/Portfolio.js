2454c542c16492f92b8cdf4ae33ff870508b10d4
Sai Purciful <saipurciful@gmail.com>
2021-02-11 12:36:40 -0800

    add portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,20 +1,88 @@
 import React from 'react'
-
+import { FaGithub, FaChrome } from "react-icons/fa"
+import CardDeck from 'react-bootstrap/CardDeck'
+import Card from 'react-bootstrap/Card'
 function Portfolio() {
     return (
-        <div>
-            <div class="thumb" data-id="JS/HTML/CSS">
-                <img src="pictures/burger.png" alt="" />
+        <section className="port">
 
-                <h3>Eat-Da-Burger <a target="_blank" href="https://github.com/Saipurciful/burger"><i
-                    class="fab fa-github"></i>
-                    Code</a>
-                    <a target="_blank" href="https://evening-wildwood-75697.herokuapp.com/"><i class="fab fa-chrome"></i>
-                    Website</a></h3>
+
+            <div className="thumb">
+                <img src="burger.png" alt="" />
+
+                <h5>JS: Eat-Da-Burger <a target="_blank" href="https://github.com/Saipurciful/burger"> <FaGithub />
+                     Code</a>
+                    <a target="_blank" href="https://evening-wildwood-75697.herokuapp.com/"><FaChrome />
+                    Website</a></h5>
 
             </div>
-        </div>
+
+            <div className="thumb" data-id="JS/HTML/CSS">
+                <img src="calendar.png" alt="" />
+
+                <h5>JS: Daily Calendar <a target="_blank" href="https://github.com/Saipurciful/DailyCalendar"><FaGithub />
+                         Code</a>
+                    <a target="_blank" href="https://saipurciful.github.io/DailyCalendar/"><FaChrome />
+                         Website</a></h5>
+
+            </div>
+
+            <div className="thumb" data-id="JS/HTML/CSS">
+                <img src="cocktail.png" alt="" />
+
+                <h5>JS: Meals Generator <a target="_blank" href="https://github.com/samrod777/Recipe-List-Platform"><FaGithub />
+                    Code</a>
+                    <a target="_blank" href="https://samrod777.github.io/Recipe-List-Platform/"><FaChrome />
+                    Website</a></h5>
+
+
+            </div>
+
+            <div className="thumb" data-id="JS/HTML/CSS">
+                <img src="codeq.png" alt="" />
+                <h5>JS: CodeQuiz <a target="_blank" href="https://github.com/Saipurciful/JS-CodeQuiz"><FaGithub />
+                    Code</a>
+                    <a target="_blank" href="https://saipurciful.github.io/JS-CodeQuiz/"><FaChrome />
+                    Website</a></h5>
+
+            </div>
+
+            <div className="thumb" data-id="JS/HTML/CSS">
+                <img src="noteTaker.png" alt="" />
+                <h5>JS: Note Taker <a target="_blank" href="https://github.com/Saipurciful/Note-Taker"><FaGithub />
+                    Code</a>
+                    <a target="_blank" href="https://notetakerapplic.herokuapp.com/"><FaChrome />
+                    Website</a></h5>
+
+            </div>
+
+            <div className="thumb" data-id="JS/HTML/CSS">
+                <img src="pwgen.png" alt="" />
+                <h5>JS: PW-Generator <a target="_blank" href="https://github.com/Saipurciful/Password-Generator"><FaGithub /> Code</a>
+                    <a target="_blank" href="https://saipurciful.github.io/Password-Generator/"><FaChrome />Website</a></h5>
+            </div>
+
+            <div className="thumb" data-id="Handlebars">
+                <img src="caffee.png" alt="" />
+                <h5>HANDLEBAR: Caffeine <a target="_blank" href="https://github.com/aprilyanggarwood/CaffeinParadise2/tree/Sai"><FaGithub /> Code</a>
+                    <a target="_blank" href="https://caffeinparadise2.herokuapp.com/signup.html"><FaChrome /> Website</a></h5>
+            </div>
+
+            <div className="thumb" data-id="Mern">
+                <img src="/56-imageGalleryHoverEffects/noteTaker.png" alt="COMMING SOON" />
+                <h5><FaGithub />MERN: Code
+                <FaChrome /> Website</h5>
+            </div>
+            <div className="thumb" data-id="Mern">
+                <img src="/56-imageGalleryHoverEffects/noteTaker.png" alt="COMMING SOON" />
+                <h5><FaGithub />MERN: Code
+                <FaChrome /> Website</h5>
+            </div>
+        </section>
+
+
     )
+
 }
 
 export default Portfolio
