48af7d2ac195cac83a83c6006f1c0ebb48464aa0
Sai Purciful <saipurciful@gmail.com>
2021-02-12 18:34:56 -0800

    fix route in App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-// import "./App.css"
-import React from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import { Home, About, Contact, Portfolio, Resume } from "./components/pages"
-import Navbar from "./components/pages/Navbar"
-import Footer from "./components/pages/Footer"
+
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Navbar from "./components/pages/Navbar";
+import Footer from "./components/pages/Footer";
+import Contact from "./components/pages/Contact";
+import Resume from "./components/pages/Resume";
+import Portfolio from "./components/pages/Portfolio";
 
 function App() {
 
   return (
-
     <Router>
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/about">
-          <About />
-        </Route>
-        <Route exact path="/contact">
-          <Contact />
-        </Route>
-        <Route exact path="/portfolio">
-          <Portfolio />
-        </Route>
-        <Route exact path="/resume">
-          <Resume />
-        </Route>
-      </Switch>
-      <Footer />
+      <div>
+        <Navbar />
+
+
+        <Route exact path="/" component={Home} />
+
+
+        <Route exact path="/About" component={About} />
+
+
+        <Route exact path="/Contact" component={Contact} />
+
+        <Route exact path="/Portfolio" component={Portfolio} />
+
+        <Route exact path="/Resume" component={Resume} />
+
+        <Footer />
+
+      </div>
     </Router>
 
   )
--- src/components/pages/index.js
+++
@@ -1,8 +0,0 @@
-import Home from "./Home"
-import About from "./About"
-import Contact from "./Contact"
-import Portfolio from "./Portfolio"
-import Resume from "./Resume"
-
-
-export { Home, About, Contact, Portfolio, Resume }
