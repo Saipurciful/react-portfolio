ba61b2a1bb8ea21dda410d4b79617211bb16de7e
Sai Purciful <saipurciful@gmail.com>
2021-02-12 18:44:50 -0800

    fix route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
