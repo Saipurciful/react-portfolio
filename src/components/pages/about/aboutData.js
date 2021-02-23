e3a6e7a431fc3dda626f6f36f5eec945c06e29e3
Sai Purciful <saipurciful@gmail.com>
2021-02-23 10:37:51 -0800

    fix footer

---
+++ src/components/pages/about/aboutData.js
@@ -0,0 +1,38 @@
+import React from 'react';
+export const url = [
+    {
+        order: 3,
+        title: 'About Me',
+        dates: "",
+        duties: ['Successful professional with a bachelor’s degree in hotel management, two years of experience in large-scale hotel management, and three years of experience as a licensed massage therapist.',
+            'Seeking a career change to a full-stack web developer for a fast-paced company with big goals that I can be a part of. Driven self-starter and lifelong learner who tackles difficult tasks with a team-player attitude.'],
+        company: 'ABOUT ME'
+    },
+    {
+        order: 2,
+        title: 'Work Experience',
+        dates: "",
+        duties: [
+            '10/2018 to Present, Massage Envy, Bellevue, Washington',
+            'High-energy massage therapist with wild-rage knowledge of massage methods',
+            '05/2017 to 09/2018, Massage Envy, Las Vegas, NV',
+            '06/2017 to 09/2018, Nuture Spa and Salon, Luxor Hotel & Casino, Las Vegas, NV',
+            '05/2015 to 06/2017, HouseKeeping Supervisor, The Service Companies', 'The Linq Hotel, Las Vegas, NV, Manage and coach up to 30 employees in the HouseKeeping Department and collaborate with Engineering, Front Desk and other departments to ensure an smooth operation of all 2,640 guest rooms.'
+        ],
+        company: 'WORK EXPERIENCE'
+    },
+    {
+        order: 1,
+        title: 'Education',
+        dates: "",
+        duties: [
+            '08/2020 to 02/2012, Certificate, Full Stack Web Development, Seattle, Washington',
+            '05/2016 to 02/2017, Associate, Massage Therapist, Las Vegas, NV',
+            '2002 to 2005, Hotel Management, Bangkok, Thailand'
+        ],
+        company: 'EDUCATION'
+    },
+
+];
+
+
